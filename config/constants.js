9702817850d56946f3a66e183067873a1f1ed6b3
nehaaggarwal1311 <neha.aggarwal1311@gmail.com>
2020-06-19 23:41:06 -0400

    Add files via upload

---
+++ config/constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+	constants: {
+		BACKUP_DB_URL: "mongodb+srv://admin:example@cluster0-kppzy.mongodb.net/testdb?retryWrites=true&w=majority",
+		DB_COLLECTION_NAME: "Cards",
+		DB_NAME: "PokemonBackupDb",
+		API_SERVER_PORT: 5000
+	},
+	FEEDBACK_MESSAGE: {
+		
+	}
+}
